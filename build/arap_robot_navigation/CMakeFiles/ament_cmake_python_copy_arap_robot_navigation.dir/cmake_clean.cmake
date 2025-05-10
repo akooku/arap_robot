98212596740f843723fd9ef792c2f27cd5941c2f
akooku <akooku12@gmail.com>
2025-05-10 23:30:08 +0000

    Remove unecessary files

--- build/arap_robot_navigation/CMakeFiles/ament_cmake_python_copy_arap_robot_navigation.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ament_cmake_python_copy_arap_robot_navigation"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ament_cmake_python_copy_arap_robot_navigation.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/arap_robot_navigation/ament_cmake_python/arap_robot_navigation/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='arap_robot_navigation',
-    version='0.0.0',
-    packages=find_packages(
-        include=('arap_robot_navigation', 'arap_robot_navigation.*')),
-)
